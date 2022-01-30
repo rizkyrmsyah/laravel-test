580f11133b0a6b2a63b4798ab3f29df85073390c
Muhamad Rizky Rahmansyah <rizkyrmsyah@gmail.com>
2022-01-30 09:58:10 +0700

    add unit test user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserTypeEnum;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
@@ -16,24 +16,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'phone' => '0880123123123',
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => 'test123',
+            'credit' => 20,
+            'type' => UserTypeEnum::regular(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\UserTypeEnum;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $data = [
+        'name' => 'rizkytest',
+        'email' => 'rizkyrmsyah@gmail.com',
+        'phone' => '0880123123123',
+        'password' => '123123',
+        'password_confirmation' => '123123',
+    ];
+
+    public function test_register_regular_user()
+    {
+        $data = $this->data;
+        $data['type'] = UserTypeEnum::regular();
+
+        $response = $this->post('/api/user/register', $data);
+        $response->assertStatus(Response::HTTP_CREATED);
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+            'type' => UserTypeEnum::regular(),
+            'credit' => 20
+        ]);
+    }
+
+    public function test_register_premium_user()
+    {
+        $data = $this->data;
+        $data['type'] = UserTypeEnum::premium();
+
+        $response = $this->post('/api/user/register', $data);
+        $response->assertStatus(Response::HTTP_CREATED);
+        $this->assertDatabaseHas('users', [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+            'type' => UserTypeEnum::premium(),
+            'credit' => 40
+        ]);
+    }
+
+    public function test_login()
+    {
+        $user = User::factory()->create();
+        $data = [
+            'phone' => $user->phone,
+            'password' => 'test123',
+        ];
+
+        $response = $this->post('/api/user/login', $data);
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_authenticated_user()
+    {
+        $user = User::factory()->create();
+        $data = [
+            'phone' => $user->phone,
+            'password' => 'test123',
+        ];
+        $user = $this->post('/api/user/login', $data);
+
+        $accessToken = json_decode($user->getContent(), true)['data']['access_token'];
+        $response = $this->withHeaders(['Authorization' => 'Bearer ' . $accessToken])->get('/api/user/show-profile');
+        $response->assertStatus(Response::HTTP_OK);
+    }
+}
